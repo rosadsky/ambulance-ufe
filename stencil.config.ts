6f129c1203ead8d61c003bda853bd612eda13fc0
Roman Osadsky <rmnosadsky@gmail.com>
2025-03-04 11:22:56 +0100

    fix: unit tests

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,26 +2,26 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'ambulance-ufe',
-   globalScript: 'src/global/app.ts', 
- outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
+   globalScript: 'src/global/app.ts',
+   outputTargets: [
+      {
+        type: 'dist',
+        esmLoaderPath: '../loader',
+      },
+      {
+        type: 'dist-custom-elements',
+        customElementsExportBehavior: 'auto-define-custom-elements',
+        externalRuntime: false,
+      },
+      {
+        type: 'docs-readme',
+      },
+      {
+        type: 'www',
+        serviceWorker: null, // disable service workers
+      },
+    ],
+    testing: {
+      browserHeadless: "shell",
     },
-    {
-      type: 'dist-custom-elements',
-      customElementsExportBehavior: 'auto-define-custom-elements',
-      externalRuntime: false,
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
-  ],
-  testing: {
-    browserHeadless: "shell",
-  },
 };
