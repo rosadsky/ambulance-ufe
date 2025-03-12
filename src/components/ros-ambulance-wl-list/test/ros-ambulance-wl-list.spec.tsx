30aeb4844a2426c9c895716d89a885bd85df6878
Roman Osadsky <rmnosadsky@gmail.com>
2025-03-12 20:19:23 +0100

    Added ambulance waiting list API

--- src/components/ros-ambulance-wl-list/test/ros-ambulance-wl-list.spec.tsx
+++ src/components/ros-ambulance-wl-list/test/ros-ambulance-wl-list.spec.tsx
@@ -1,18 +1,81 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { RosAmbulanceWlList } from '../ros-ambulance-wl-list';
+import { WaitingListEntry } from '../../../api/ambulance-wl/models';
+import fetchMock from 'jest-fetch-mock';
+
 
 describe('ros-ambulance-wl-list', () => {
-  it('renders', async () => {
+
+  const sampleEntries: WaitingListEntry[] = [
+    {
+      id: "entry-1",
+      patientId: "p-1",
+      name: "Juraj Prvý",
+      waitingSince: new Date("20240203T12:00"),
+      estimatedDurationMinutes: 20
+    },
+    {
+      id: "entry-2",
+      patientId: "p-2",
+      name: "James Druhý",
+      waitingSince: new Date("20240203T12:00"),
+      estimatedDurationMinutes: 5
+    }
+  ];
+
+  beforeAll(() => {
+    fetchMock.enableMocks();
+  });
+
+  afterEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  it('renders sample entries', async () => {
+
+    fetchMock.mockResponseOnce(JSON.stringify(sampleEntries));
+
     const page = await newSpecPage({
       components: [RosAmbulanceWlList],
-      html: `<ros-ambulance-wl-list></ros-ambulance-wl-list>`,
+        html: `<ros-ambulance-wl-list ambulance-id="test-ambulance" api-base="http://test/api"></ros-ambulance-wl-list>`,
     });
+
+    // Wait for the DOM to update
+    await page.waitForChanges();
 
       const wlList = page.rootInstance as RosAmbulanceWlList;
       const expectedPatients = wlList?.waitingPatients?.length
 
       const items = page.root.shadowRoot.querySelectorAll("md-list-item");
+
+    // Assert that the expected number of patients and rendered items match the sample entries
+    expect(expectedPatients).toEqual(sampleEntries.length);
       expect(items.length).toEqual(expectedPatients);
    });
+
+  it('renders error message on network issues', async () => {
+    // Mock the network error
+    fetchMock.mockRejectOnce(new Error('Network Error'));
+
+    const page = await newSpecPage({
+      components: [RosAmbulanceWlList],
+        html: `<ros-ambulance-wl-list ambulance-id="test-ambulance" api-base="http://test/api"><ros-ambulance-wl-list>`,
+    });
+
+    const wlList = page.rootInstance as RosAmbulanceWlList;
+    const expectedPatients = wlList?.waitingPatients?.length;
+
+    // Wait for the DOM to update
+    await page.waitForChanges();
+
+    // Query the DOM for error message and list items
+    const errorMessage = page.root.shadowRoot.querySelectorAll(".error");
+    const items = page.root.shadowRoot.querySelectorAll("md-list-item");
+
+    // Assert that the error message is displayed and no patients are listed
+    expect(errorMessage.length).toBeGreaterThanOrEqual(1);
+    expect(expectedPatients).toEqual(0);
+    expect(items.length).toEqual(expectedPatients);
+  });
 });
 
